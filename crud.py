8902f84c97f0549bb0202db486d32473fa089ed4
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-07-30 01:11:18 +0900

    ✨ feat: 회원가입 기능 추가

--- crud.py
+++ crud.py
@@ -1,15 +1,16 @@
 from sqlalchemy.orm import Session
-import model
+from model import Member
 import scheme
 import bcrypt
 
 
-def create_member(member: scheme.SignUpInfo):
-    print(member.email)
-    print(bcrypt.hashpw(member.password.encode('utf-8'), bcrypt.gensalt()))
-    db_member = model.Member(email=member.email,
-                             password=bcrypt.hashpw(member.password.encode('utf-8'), bcrypt.gensalt()))
-    Session.add(db_member)
-    Session.commit()
-    Session.refresh(db_member)
+def create_member(db: Session, member: scheme.SignUpInfo):
+    db_member = Member(
+        email=member.email,
+        password=bcrypt.hashpw(member.password.encode('utf-8'), bcrypt.gensalt())
+    )
+    print(db_member.email)
+    db.add(db_member)
+    db.commit()
+    db.refresh(db_member)
     return db_member
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+
 import scheme
 import crud
-from sqlalchemy.orm import Session
+import database
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
@@ -12,8 +31,8 @@ async def root():
 
 
 @app.post("/signup")
-async def member_sign_up(req: scheme.SignUpInfo):
-    member = crud.create_member(member=req)
+async def member_sign_up(req: scheme.SignUpInfo, db: Session = Depends(get_db)):
+    member = crud.create_member(member=req, db=db)
     return {
         "message": "Sign Up Request Successed.",
         "email": str(member.email)
