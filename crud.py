28177ec0d3cdeda52b266617c67d36e9c3ba2d88
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-07-31 23:14:00 +0900

    🎨 style: reformat code

--- crud.py
+++ crud.py
@@ -6,10 +6,10 @@ import bcrypt
 
 
 def create_member(db: Session, member: scheme.MemberSignInfo):
-    haswed_password = bcrypt.hashpw(member.password.encode('utf-8'), bcrypt.gensalt())
+    hashed_password = bcrypt.hashpw(member.password.encode('utf-8'), bcrypt.gensalt())
     db_member = Member(
         email=member.email,
-        password=haswed_password.decode('utf-8')
+        password=hashed_password.decode('utf-8')
     )
     print(db_member.email)
     db.add(db_member)
@@ -19,13 +19,13 @@ def create_member(db: Session, member: scheme.MemberSignInfo):
 
 
 def find_member_by_email(db: Session, member: scheme.MemberSignInfo):
-    db_member = db.query(Member).filter(
-        member.email == Member.email
-    )
+    db_member = db.query(Member).filter(member.email == Member.email)
     if db_member:
         for m in db_member:
             return m
     else:
         return None
+
+
 def find_region_list(db: Session):
     return db.query(Region).all()
