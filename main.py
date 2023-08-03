222b29f790cdf9a88ef02b47d2204c0fd2abc53f
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-08-03 19:44:20 +0900

    ✨ feat: CORS origin에 localhost:3000 추가

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*", "http://localhost:3000"],
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*", "access-token", "access_token"],
     allow_credentials=True
 )
 
